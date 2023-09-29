6d255ced09a03da9e2274dfcb22b72ded2cdd397
Pavel <black_yaguar@mail.ru>
2023-09-29 11:26:29 +0500

    Создан класс ослы

---
+++ nurseryanimal/Donkey.java
@@ -0,0 +1,10 @@
+package nurseryanimal;
+
+public class Donkey extends Packanimal {
+
+    public Donkey(String name, String command, String birthDate, String packAnimalView) {
+        super(name, command, birthDate, packAnimalView);
+       
+    
+}
+}
