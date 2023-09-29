58aad8c6bb536256689b6f3f2a9e2327424e00a2
Pavel <black_yaguar@mail.ru>
2023-09-29 11:25:19 +0500

    Создан класс лошади

---
+++ nurseryanimal/Horse.java
@@ -0,0 +1,10 @@
+package nurseryanimal;
+
+public class Horse  extends Packanimal{
+
+    public Horse(String name, String command, String birthDate, String packAnimalView) {
+        super(name, command, birthDate, packAnimalView);
+        
+    }
+    
+}
