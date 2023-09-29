a9fef26cf2f25f218f1acd42690235a5b533603d
Pavel <black_yaguar@mail.ru>
2023-09-29 10:56:57 +0500

    Создание класса собака

---
+++ nurseryanimal/Dog.java
@@ -0,0 +1,9 @@
+package nurseryanimal;
+
+public class Dog extends Pet {
+    public Dog (String name, String command, String birthDate, String petView) {
+        super(name, command, birthDate, petView);
+    }
+
+    
+}
