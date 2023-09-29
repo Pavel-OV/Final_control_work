14d46699eb849a7edd5ade7b95a8bde86fdc1681
Pavel <black_yaguar@mail.ru>
2023-09-29 11:26:00 +0500

    Создан класс верблюды

---
+++ nurseryanimal/Camel.java
@@ -0,0 +1,12 @@
+package nurseryanimal;
+
+public class Camel  extends  Packanimal {
+
+    public Camel(String name, String command, String birthDate, String packAnimalView) {
+        super(name, command, birthDate, packAnimalView);
+       
+    }
+
+
+    
+}
