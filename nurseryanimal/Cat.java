cedba86ca7408dcc49fd5e7e5a119729d8de43cc
Pavel <black_yaguar@mail.ru>
2023-09-29 10:56:21 +0500

    Создание класса кошка

---
+++ nurseryanimal/Cat.java
@@ -0,0 +1,8 @@
+package nurseryanimal;
+
+public class Cat extends Pet {    
+    public Cat(String name, String command, String birthDate, String petView) {
+        super(name, command, birthDate, petView);
+    }
+
+}
