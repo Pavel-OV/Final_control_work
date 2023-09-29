4188558d3350d30c4cabf266c6f345baf30221e6
Pavel <black_yaguar@mail.ru>
2023-09-29 10:57:30 +0500

    Создание класса хомяки

---
+++ nurseryanimal/Hamster.java
@@ -0,0 +1,9 @@
+package nurseryanimal;
+
+public class Hamster extends  Pet {
+
+    public Hamster(String name, String command, String birthDate, String petView) {
+        super(name, command, birthDate, petView);
+    }
+    
+}
